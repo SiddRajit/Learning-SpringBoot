2f655bc223bf6bfa3ab3789bb810db62c807add8
Siddharth Rajit <siddharth.rajit2207@gmail.com>
2026-07-16 13:31:00 -0300

    -Create a new spring boot project and add tests for json serilizaton and deserialization. -Learn about Test driven devellopment.

---
+++ CashCardAPI/src/main/java/com/FamilyCashCardApp/spring_rest_api/CashCard.java
@@ -0,0 +1,5 @@
+package com.FamilyCashCardApp.spring_rest_api;
+
+public record CashCard(Long id, Double amount) {
+
+}
---
+++ CashCardAPI/src/test/java/com/FamilyCashCardApp/spring_rest_api/CashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FamilyCashCardApp.spring_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
